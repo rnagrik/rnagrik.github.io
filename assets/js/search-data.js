4834d12138bbd951506a2d5442cf908744f1e542
rnagrik <152533042+rnagrik@users.noreply.github.com>
2025-09-26 22:40:44 +0000

    Deploying to gh-pages from @ rnagrik/rnagrik.github.io@1158375fe650c72e24f5a69740675c309b24d370 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,19 +9,19 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
-          title: "cv",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "nav-projects",
+  },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
+          },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
           },
         },{id: "projects-project-1",
           title: 'project 1',
