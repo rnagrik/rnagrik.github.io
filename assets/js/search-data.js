9a9f01d632e8f4aff397f29d6ab1563857a15a87
rnagrik <152533042+rnagrik@users.noreply.github.com>
2025-09-27 19:07:22 +0000

    Deploying to gh-pages from @ rnagrik/rnagrik.github.io@ab8c79d71f25c12a969ac171ab65b4f4051aacbd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,14 +28,14 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-lidar-slam-with-pose-graph-optimization",
+          title: 'LiDAR SLAM with Pose Graph Optimization',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
