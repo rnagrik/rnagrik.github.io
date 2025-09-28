28bbd59667fa847f158fdef20b4d2ef811087588
rnagrik <152533042+rnagrik@users.noreply.github.com>
2025-09-28 08:34:13 +0000

    Deploying to gh-pages from @ rnagrik/rnagrik.github.io@ed0a903023e06c0a8410ef206ce54bf740911a75 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -43,9 +43,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-motion-planning-with-a-and-rrt-connect",
+          title: 'Motion Planning with A* and RRT-Connect',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
